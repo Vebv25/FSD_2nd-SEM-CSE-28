a143e610abf16b0475204cc5476cd5bc6ecd8a8b
vebv25 <vaibhav.25b01010535@abes.ac.in>
2026-08-24 10:51:12 +0530

    done

---
+++ oops/exp14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+int a = 10, b = 20;
+cout << "Before swap: " << a << " " << b << endl;
+swap(a, b);
+cout << "After swap: " << a << " " << b << endl;
+return 0;
+}
