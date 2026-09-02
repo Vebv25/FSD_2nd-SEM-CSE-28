3abe345829e9e1098a8a8b4ce44890993245b0f8
vebv25 <vaibhav.25b01010535@abes.ac.in>
2026-09-02 09:59:46 +0530

    done

--- adv dsa/exp11.cpp
+++ adv dsa/exp11.cpp
@@ -1,2 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
 using namespace std;
+
+int main() {
+    stack<int> s;
+    int n, a = 0, b = 1, c;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    s.push(a);
+    s.push(b);
+
+    for (int i = 2; i < n; i++) {
+        c = a + b;
+        s.push(c);
+        a = b;
+        b = c;
+    }
+
+    cout << "Fibonacci series: ";
+
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
